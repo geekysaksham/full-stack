78a2980c1ef284aabbc03dc740b21d253507ca3f
Saksham Jain <geekysaksham01@gmail.com>
2026-08-19 00:20:18 +0530

    Lec-29 js foundation

---
+++ WebDev cw/JavaScript/Lec-29 js foundation/index.js
@@ -0,0 +1,98 @@
+// console.log("Hello World");
+// console.log("4 * 5 =",4*5);
+
+// name = "Saksham Jain";
+// age = 19;
+// console.log(name);
+// console.log(age);
+// console.log(name, age);
+
+// var, let and const
+// var age; // declaration -> box created without value
+// age = 19; // initialization -> box mei value de di
+// var age = 19; // both together declaration and initialization
+
+// var age = 19;
+// console.log(age);
+// var age = 23; // re-declaration
+// age = 26; // updation
+// console.log(age);
+
+// let name = "Saksham";
+// console.log(name);
+// // let name = "Jain"; // re-declaration not allowed
+// name = "Jain"; // updation allowed
+// console.log(name);
+
+// const id = "05015602725";
+// console.log(id);
+// const id = "05015602725"; // re-declaration not allowed
+// id = "05015602725"; // updation not allowed
+
+//Variable Naming Rules
+// let $price = 5000;
+// let _crazy = "Saksham";
+// let age = 23;
+
+// let @name = "hello"; Wrong Naming Rules
+// let const = 4; Wrong as contain reserved Keywords
+
+// let LET = "hii";
+// console.log(LET);
+
+//Camel Case for js
+// let firstName = "Tanmay";
+// console.log(firstName);
+
+//Snake Case for python
+// let first_name = "Tanay";
+// console.log(first_name);
+
+//Pascal Case
+// let FirstName = "Tanay";
+// console.log(FirstName);
+
+//SCREAMING_SNAKE
+// let DB_URL = "xyz";
+// console.log(DB_URL);
+
+// let collegeName = "London Academy";
+// let mobNumber = 9876543210;
+// let discount = 56.23;
+// console.log(typeof collegeName); //string
+// console.log(typeof mobNumber); //number
+// console.log(typeof(mobNumber)); //number
+// console.log(typeof discount); //number
+// //Type Change kr skte hai
+// discount = "hello";
+// console.log(typeof discount); //string
+
+//Single,Double Quotes Same
+// const doubleQuote = "Hello";
+// const singleQuote = 'Hii';
+// console.log(doubleQuote);
+// console.log(singleQuote);
+
+// //MultiLine Template Literal
+// const templateLiteral = `Hi Friends
+// Kese Ho Tum`;
+// console.log(templateLiteral);
+
+// const userName = "Riyaz";
+// const greetMess = `Hii, ${userName}`;
+// const greetMess = `Hii, ${3+8}`;
+// const greetMess = `Hii, ${console.log("Hii, Comfy")}`;
+// console.log(greetMess);
+
+//Undefined
+// let user;
+// console.log(user);
+// console.log(typeof undefined); //undefined
+
+//null
+// let products = null;
+// console.log(typeof products); //Object
+
+// boolean
+let loggedIn = false;
+console.log(typeof loggedIn); //boolean
