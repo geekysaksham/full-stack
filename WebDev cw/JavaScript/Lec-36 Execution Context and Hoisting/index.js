ef199932720a20b85b7cc10bb4a5152c597c9d8a
Saksham Jain <geekysaksham01@gmail.com>
2026-09-01 18:38:54 +0530

    Lec-36 Execution Context and Hoisting

---
+++ WebDev cw/JavaScript/Lec-36 Execution Context and Hoisting/index.js
@@ -0,0 +1,77 @@
+// console.log(a);
+// var a = 5;
+
+// console.log(b);
+// let b = 5;
+
+// fun1();
+// function fun1() {
+//     console.log("Hii");
+// }
+
+// fun2();
+// var fun2 = function() {
+//     console.log("Hello");
+// }
+
+// fun3();
+// const fun3 = function() {
+//     console.log("Hii Guyz");
+// }
+
+// var a = 5;
+// let b = 10;
+// console.log(a + b);
+
+// function fun1() {
+//     console.log("Hii");
+// }
+// fun1();
+
+// var a = 5;
+// let b = 10;
+// console.log(a + b);
+
+// function fun1() {
+//     let num1 = 10;
+//     let num2 = 20;
+//     return num1 + num2;
+// }
+
+// const result = fun1();
+// console.log(result);
+
+// var a = 5;
+// let b = 10;
+// console.log(a + b);
+
+// function outter() {
+//     let num1 = 10;
+//     let num2 = 20;
+
+//     function inner() {
+//         let num1 = 50;
+//         let num2 = 60;
+//         return num1 + num2;
+//     }
+
+//     const result = inner() + num1 + num2;
+//     return result;
+// }
+
+// const result = outter();
+// console.log(result);
+
+// function recurse() {
+//     recurse();
+// }
+
+// recurse();
+
+let a = 10;
+function gun() {
+    console.log(a);
+    let a = 20;
+}
+
+gun();
